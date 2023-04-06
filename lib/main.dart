f23a8f87c953f276d8713c2f15cf473e86edbe68
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-04-06 14:45:44 +0300

    created separate widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,22 +17,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Pants',
-      amount: 14.33,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'Sneakers',
-      amount: 33.99,
-      date: DateTime.now(),
-    ),
-  ];
-
-  MyHomePage({super.key});
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +29,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(
+            Container(
               width: double.infinity,
               child: Card(
                 color: Colors.green,
@@ -53,76 +37,7 @@ class MyHomePage extends StatelessWidget {
                 child: Text('CHART!'),
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: const EdgeInsets.all(2),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                    ),
-                    const TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.indigoAccent,
-                      ),
-                      onPressed: null,
-                      child: const Text('Add transaction'),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Column(
-                children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.indigoAccent,
-                          width: 2,
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        '\$ ${tx.amount}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.indigoAccent,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: const TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: const TextStyle(color: Colors.brown),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList())
+            UserTransaction()
           ],
         ));
   }
---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import './new_transaction.dart';
+import './transaction_list.dart';
+
+class UserTransaction extends StatefulWidget {
+  const UserTransaction({super.key});
+
+  @override
+  State<UserTransaction> createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Pants',
+      amount: 14.33,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Sneakers',
+      amount: 33.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransaction),
+      ],
+    );
+  }
+}
