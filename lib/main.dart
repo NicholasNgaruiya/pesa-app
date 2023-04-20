0e9889dd5637918771311dd0cacafd02be9a3428
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-04-20 14:08:36 +0300

    Added some styling

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Expense Tracker',
       home: const MyHomePage(),
       theme: ThemeData(
         primarySwatch: Colors.green,
